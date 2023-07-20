15562a4fb88cafae17146606fe0e424bad5f93ad
Zaim Beshich <zaimbeshich93@gmail.com>
2023-07-20 21:41:10 +0400

    add constants

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+  white: '#FFFFFF',
+  black: '#1E1E20',
+  purple: '#A09EFF',
+  grey: '#60626D',
+  light: '#F3F4FB',
+  blue: '#413DFF',
+  red: '#FF450B',
+};
+export default COLORS;
---
+++ src/constants/fonts.js
@@ -0,0 +1,4 @@
+export const Raleway_600 = 'Raleway-SemiBold';
+export const Raleway_500 = 'Raleway-Medium';
+export const Raleway_400 = 'Raleway-Regular';
+export const Raleway_300 = 'Raleway-Light';
