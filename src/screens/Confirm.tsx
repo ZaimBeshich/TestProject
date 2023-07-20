5518cfcac8deecee3b2a95cdcdd46dbcc341304b
Zaim Beshich <zaimbeshich93@gmail.com>
2023-07-20 22:33:23 +0400

    add confirm screen

---
+++ src/screens/Confirm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+export const Confirm = ({ navigation }) => {
+  return (
+    <View>
+      <Text>CONFIRM</Text>
+    </View>
+  );
+};
